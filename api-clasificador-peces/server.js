134372d7f3a10ed0ce0a858208a1ea41efb9a853
yjot2 <2010guabo@gmail.com>
2026-06-06 18:21:41 -0500

    Mejoras de codigo v2.3

--- api-clasificador-peces/server.js
+++ api-clasificador-peces/server.js
@@ -2,22 +2,13 @@ import expressRateLimit from "express-rate-limit";
 import express from "express";
 import compression from "compression";
 import { api1 } from "./src/routes/clasificadorRoute.js";
-
-const isProduction = process.env.NODE_ENV === "production"; // Detectar entorno al iniciar el servidor
-
-// Cargar variables de entorno solo en desarrollo
-if (!isProduction) {
-  const dotenv = await import("dotenv");
-  dotenv.config();
-}
+import { port, isProduction } from "./src/env/config.js";
 
 const dailyLimiter = expressRateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 horas
   max: 80, // máximo 80 requests por IP al día
   message: "Has alcanzado el límite diario de solicitudes."
 });
-
-const PORT = process.env.PORT; // Configurar puerto dinámico
 
 const app = express()
 
@@ -37,12 +28,12 @@ app.get("/", (_, res) => {
 app.use("/api", dailyLimiter, api1);
 
 // Iniciar el servidor
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(port, "0.0.0.0", () => {
   if (isProduction) {
     // Remoto: solo HTTP, el servidor ya da HTTPS
-    console.log(`Servidor corriendo en producción en puerto ${PORT}`);
+    console.log(`Servidor corriendo en producción en puerto ${port}`);
   } else {
     // Local: solo HTTP, el servidor no requiere HTTPS (no es buena practica)
-    console.log(`Servidor corriendo en desarrollo en puerto ${PORT}`);
+    console.log(`Servidor corriendo en desarrollo en puerto ${port}`);
   }
 });
---
+++ api-clasificador-peces/src/env/config.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = await import("dotenv");
+  dotenv.config();
+}
+
+export const port = process.env.PORT; // Configurar puerto dinámico
+export const isProduction = process.env.NODE_ENV === "production"; // Detectar entorno al iniciar el servidor
